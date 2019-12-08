b6367cb4d3f62c268c4a9dee1ffe22c487b41021
JulieArt <Julia.biryukova.2016@gmail.com>
2019-12-08 18:07:45 +0300

    3 lesson

---
+++ testSnake/Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace testSnake
+{
+    public class Point
+    {
+        internal int x;
+        internal int y;
+        internal char sym;
+
+        public Point()
+        {
+
+        }
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
---
+++ testSnake/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace testSnake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            Draw(p2.x, p2.y, p2.sym);
+
+
+            Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
